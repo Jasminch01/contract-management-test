3eac695466e06a4dcea4179ce5fbd3db53d1b9a5
jasmin <chjasmin27@gmail.com>
2025-08-07 09:45:03 +0600

    cookie updated

--- middelwares/authMiddleware.js
+++ middelwares/authMiddleware.js
@@ -1,8 +1,24 @@
+// const jwt = require("jsonwebtoken");
+// const SECRET = process.env.JWT_SECRET || "supersecret";
+
+// module.exports = (req, res, next) => {
+//   const token = req.cookies.accesstoken;
+//   if (!token) return res.status(401).json({ message: "No token provided" });
+
+//   try {
+//     const decoded = jwt.verify(token, SECRET);
+//     req.userId = decoded.id;
+//     next();
+//   } catch (err) {
+//     res.status(401).json({ message: "Invalid token" });
+//   }
+// };
+
 const jwt = require("jsonwebtoken");
 const SECRET = process.env.JWT_SECRET || "supersecret";
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.accesstoken;
+  const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "No token provided" });
 
   try {
