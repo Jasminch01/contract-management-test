5eaa8d52f183bd8cf46869a000513eb5b96bc8b2
jasmin <chjasmin27@gmail.com>
2025-09-19 17:42:16 +0600

    fix buyer and seller info

--- controllers/contract.controller.js
+++ controllers/contract.controller.js
@@ -112,80 +112,6 @@ exports.getNextContractNumber = async (req, res) => {
   }
 };
 
-// @desc Get all contracts
-// exports.getAllContracts = async (req, res) => {
-//   try {
-//     const {
-//       commodity,
-//       grade,
-//       contractNumber,
-//       tonnesMin,
-//       tonnesMax,
-//       buyerId,
-//       page = 1,
-//       limit = 10,
-//       sortBy = "createdAt",
-//       sortDir = "desc",
-//     } = req.query;
-
-//     const filter = { isDeleted: false };
-
-//     if (commodity) {
-//       filter.commodity = new RegExp(commodity, "i");
-//     }
-
-//     if (grade) {
-//       filter.grade = grade;
-//     }
-
-//     if (contractNumber) {
-//       filter.contractNumber = contractNumber.toUpperCase();
-//     }
-
-//     if (buyerId && mongoose.Types.ObjectId.isValid(buyerId)) {
-//       filter.buyerId = buyerId;
-//     }
-
-//     if (tonnesMin || tonnesMax) {
-//       filter.weights = {};
-
-//       if (tonnesMin) {
-//         filter.weights.$gte = Number(tonnesMin);
-//       }
-//       if (tonnesMax) {
-//         filter.weights.$lte = Number(tonnesMax);
-//       }
-//     }
-
-//     if (req.query.status) {
-//       filter.status = req.query.status;
-//     }
-
-//     const skip = (Number(page) - 1) * Number(limit);
-//     const sort = { [sortBy]: sortDir === "asc" ? 1 : -1 };
-
-//     const [data, total] = await Promise.all([
-//       Contract.find(filter)
-//         .populate("buyer seller")
-//         .sort(sort)
-//         .skip(skip)
-//         .limit(Number(limit)),
-//       Contract.countDocuments(filter),
-//     ]);
-
-//     res.json({
-//       data: {
-//         page: Number(page),
-//         totalPages: Math.ceil(total / limit),
-//         total,
-//         data,
-//       },
-//     });
-//   } catch (error) {
-//     res.status(500).json({ message: "Error fetching contracts", error });
-//   }
-// };
-
 exports.getAllContracts = async (req, res) => {
   try {
     const {
@@ -299,11 +225,9 @@ exports.getAllContracts = async (req, res) => {
       Contract.find(filter)
         .populate({
           path: "buyer",
-          select: "name legalName email companyName", // Add fields as needed
         })
         .populate({
           path: "seller",
-          select: "name legalName email mainNgr companyName firstName lastName", // Add fields as needed
         })
         .sort(sort)
         .skip(skip)
